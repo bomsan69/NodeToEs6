ec9b3887ff376fac42859e03d6ea55aee4f0cf0b
James Chung <bomsan69@gmail.com>
2023-09-06 20:32:57 -0400

    first commit

---
+++ dist/server.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var _dotenv = _interopRequireDefault(require("dotenv"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+_dotenv["default"].config();
+console.log(process.env.API);
